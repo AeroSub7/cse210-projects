f07f844123e56507354d303dc9b232ca5a8c605f
AeroSub7 <eric.jeremiah.johnson@gmail.com>
2026-09-16 13:17:09 -0600

    Stubs complete for week 3 project

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+    }
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+    public string GetDisplayText()
+    {
+        return "reference and scripture";
+    }
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        
+    }
+    public void Hide()
+    {
+        
+    }
+    public void Show()
+    {
+        
+    }
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+    public string GetDisplayText()
+    {
+        return _text;
+    }
+}

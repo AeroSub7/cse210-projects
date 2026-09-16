11a2b5625e7262f252640c189fc9a1d202c4b6f0
AeroSub7 <eric.jeremiah.johnson@gmail.com>
2026-09-15 20:16:20 -0600

    HW week3 Encapsulation.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions x = new Fractions();
+
+        Console.WriteLine(x.GetFractionString());
+        Console.WriteLine(x.GetDecimalValue());
+
+        Fractions a = new Fractions(5);
+
+        Console.WriteLine(a.GetFractionString());
+        Console.WriteLine(a.GetDecimalValue());
+
+        Fractions b = new Fractions(3, 4);
+
+        Console.WriteLine(b.GetFractionString());
+        Console.WriteLine(b.GetDecimalValue());
+
+        Fractions c = new Fractions(1, 3);
+
+        Console.WriteLine(c.GetFractionString());
+        Console.WriteLine(c.GetDecimalValue());
+
     }
 }
